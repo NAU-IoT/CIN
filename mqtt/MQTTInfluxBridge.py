47d5d1f6d71795b14176e24398bac97c0541afb2
Akiel Aries <56521583+akielaries@users.noreply.github.com>
2022-10-26 14:08:23 -0700

    Update MQTTInfluxBridge.py

--- mqtt/MQTTInfluxBridge.py
+++ mqtt/MQTTInfluxBridge.py
@@ -24,30 +24,30 @@ MQTT_REGEX = 'home/([^/]+)/([^/]+)'
 
 influxdb_client = InfluxDBClient(INFLUXDB_ADDRESS, 8086, INFLUXDB_USER, INFLUXDB_PASSWORD, None)
 
-"""
-sensor_data class defining location, measurement, and val
-create named tuple for correct datatype
-"""
 class sensor_data_class(NamedTuple):
+    """
+    sensor_data class defining location, measurement, and val
+    create named tuple for correct datatype
+    """
     location: str
     measurement: str
     value: float
 
-"""
-function for when our client connects
-prints result code when connected and listens for our mqtt topic
-"""
 def on_connect(client, userdata, flags, result_code):
+    """
+    function for when our client connects
+    prints result code when connected and listens for our mqtt topic
+    """
     # The callback for when the client receives a CONNACK response from the server
     print('Connected with result code ' + str(result_code))
     # listen for define mosquitto topic
     client.subscribe(MQTT_TOPIC)
 
-"""
-reads variables from topic path struct and identifies vars
-returning them as the a named tuple
-"""
 def _parse_mqtt_message(topic, payload):
+    """
+    reads variables from topic path struct and identifies vars
+    returning them as the a named tuple
+    """
     # create match var = to first match of two vars passed in
     match = re.match(MQTT_REGEX, topic)
     # return named tuple if match
@@ -60,15 +60,15 @@ def _parse_mqtt_message(topic, payload):
     else:
         return None
 
-"""
-recieves variables as named tuple and creates json struct
-we can later use for visualizing our data too
-    - measurement saved directly
-    - location stored in tag struct with nested val
-    - sensor value saved under field
-    - write json struct to influx instance
-"""
 def _send_sensor_data_to_influxdb(sensor_data):
+    """
+    recieves variables as named tuple and creates json struct
+    we can later use for visualizing our data too
+        - measurement saved directly
+        - location stored in tag struct with nested val
+        - sensor value saved under field
+        - write json struct to influx instance
+    """
     json_body = [
         {
             'measurement': sensor_data.measurement,
@@ -82,11 +82,11 @@ def _send_sensor_data_to_influxdb(sensor_data):
     ]
     influxdb_client.write_points(json_body)
 
-"""
-call this function everytime a topic is published
-msg topic and payload are saved to two vars and printed to std out
-"""
 def on_message(client, userdata, msg):
+    """
+    call this function everytime a topic is published
+    msg topic and payload are saved to two vars and printed to std out
+    """
     # The callback for when a PUBLISH message is received from the server
     print(msg.topic + ' ' + str(msg.payload))
     # set var = to our named tuple vals
@@ -95,10 +95,10 @@ def on_message(client, userdata, msg):
     if sensor_data is not None:
         _send_sensor_data_to_influxdb(sensor_data)
 
-"""
-function to initialize influx db
-"""
 def _init_influxdb_database():
+    """
+    function to initialize influx db
+    """
     # set var = to get list of DBs
     databases = influxdb_client.get_list_database()
     # if the db does not exist it will be created
@@ -106,10 +106,10 @@ def _init_influxdb_database():
         influxdb_client.create_database(INFLUXDB_DATABASE)
     influxdb_client.switch_database(INFLUXDB_DATABASE)
 
-"""
-main driver function
-"""
 def main():
+    """
+    main driver function
+    """
     # intialize DB
     _init_influxdb_database()
 
@@ -127,4 +127,4 @@ def main():
 if __name__ == '__main__':
     print('MQTT to InfluxDB bridge')
     main()
-
+    
