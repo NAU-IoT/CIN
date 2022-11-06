ff085d055383b67499d5a9ce845d636465d39e6e
Akiel Aries <56521583+akielaries@users.noreply.github.com>
2022-11-06 04:11:06 -0700

    Update BME280.cpp

--- src/BME280.cpp
+++ src/BME280.cpp
@@ -2,23 +2,23 @@
  * source file used to test BME library creation
  * with C++
  */
-
+#include <Arduino.h>
 #include <Adafruit_BME280.h>
 #include "BME280.hpp"
 #include "constants.hpp"
 
-// declare instance of bme sensor
 Adafruit_BME280 bme;
 
 // initialize instance of BME for serial output display
 BME::BME(int BMEaddr) {
     if (!bme.begin(BMEaddr)) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring or default address!");
-        while (1);  
+        Serial.println("Could not find a valid BME280 sensor, check wiring or default address!");   
     }
 }
 
-void BME::printBMEToSerial() {
+void BME::collect() {
+    //pinMode(11, OUTPUT);
+    //digitalWrite(11, HIGH);
 
     // define bme readings as float vals
     // convert bme celsius reading to fahrenheit 
@@ -28,25 +28,24 @@ void BME::printBMEToSerial() {
     float _press = bme.readPressure() / 100.0;
     // convert pressure to height in feet
     float _alt = bme.readAltitude(SEALEVELPRESSURE_HPA) * 3.281;
-    
+        
     Serial.println("From BME280 Sensor: ");
     // print values to serial
     Serial.print("Temperature: ");
     Serial.print(_temp);
     Serial.println(" *F");
-    
+        
     Serial.print("Humidity: ");
     Serial.print(_hum);
     Serial.println(" %");
-    
+        
     Serial.print("Pressure: ");
     Serial.print(_press);
     Serial.println(" hPa");
-
+    
     Serial.print("Altitude: ");
     Serial.print(_alt);
     Serial.println(" ft");
-
-    Serial.println();
-    delay(5000);
+    
+    Serial.println(); 
 }
