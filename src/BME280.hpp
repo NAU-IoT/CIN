f68ed9a3343fe8cf8e4299bd295fdb7119e96ff5
Akiel Aries <56521583+akielaries@users.noreply.github.com>
2022-11-06 04:11:23 -0700

    Update BME280.hpp

--- src/BME280.hpp
+++ src/BME280.hpp
@@ -2,15 +2,14 @@
  * library created to test library usage with arduino 
  * BME280 sensor
  */
+ 
 #ifndef BME280_hpp
 #define BME280_hpp
-
-#include <Arduino.h>
 
 class BME {
     public:
         BME(int BMEaddr);
-        void printBMEToSerial();
+        void collect();
 };
 
 #endif
